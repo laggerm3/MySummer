5f1f3ff7295c4c7cff8646d08cb39f133e2998f3
German <lagunasg@outlook.com>
2015-07-05 11:29:52 -0700

    script up and running

--- parse_repmod.pl
+++ parse_repmod.pl
@@ -7,20 +7,20 @@ use warnings;
 my %sequence_of;
 
 open(INPUT_REPMOD, $ARGV[0]) or die "Cannot open the file: $!";
-my $hash_key;
+my $hash_key1;
 my $hash_value;
 while (my $line = <INPUT_REPMOD>) {
 	if ($line =~ /^>(\S+)/) {
-		$hash_key = $1;
+		$hash_key1 = $1;
 	}
 	elsif ($line =~ /\S/) {
 		chomp $line;
 		$hash_value = $line;
-		$sequence_of{$hash_key} = $hash_value;
+		$sequence_of{$hash_key1} = $hash_value;
 	} 
 }
 # prints out the name and corresponding sequence from repeat modeler
-foreach my $hash_key1 (keys %sequence_of) {
+foreach $hash_key1 (keys %sequence_of) {
 	print "$hash_key1\t$sequence_of{$hash_key1}\n";
 }
 
@@ -38,23 +38,15 @@ while (my $line2 = <INPUT_EVALUES>) {
 	}
 	elsif ($line2 =~ /\S/) {
 		chomp $line2;
-	} 
-}
-#prints out the names from the selectevalues.fa after being organized into hashes
-foreach my $hash_key3 (keys %name_of) {		
-		print "$hash_key3\n";
 	}
-
-#establishing the comparison with the repmod_results and selectevalues.fa
-
-my @common_familynames = ();
-foreach (keys %name_of) {
-	push(@common_familynames, $_) if exists $sequence_of{$_};
-###GLR: the second half of the print statement
-###GLR: calling up the sequence is giving me issues; %sequence_of{$hash_key1}
-###GLR: that is the value that is given to each key of the rep_mod file, corresponding sequence
-	print "$_\t%sequence_of{$hash_key1}\n";
+} 
+#prints out the names from the selectevalues.fa after being organized into hashes
+#prints out the corresponding sequences to the evalue met family names
+foreach my $hash_key3 (keys %name_of) {
+	if ($hash_key1 = $hash_key3) {	
+		print "Corresponding sequences: $hash_key3\t$sequence_of{$hash_key1}\n";}
 }
+
 close INPUT_REPMOD;
 close INPUT_EVALUES;
 exit;
