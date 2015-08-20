7756e75514708b97cac402010eeb109f87824b53
German <lagunasg@outlook.com>
2015-08-19 17:58:32 -0700

    working version of column switching for BED files if column 1 is greater than column 2

--- columnsswitch.pl
+++ columnsswitch.pl
@@ -3,17 +3,23 @@ use strict;
 use warnings;
 #column switching if column 2 is greater than column 3
 #adding a plus sign if that is a case so another column with a positive sign
+my @input;
 my @output;
-open (BED_FILE, $ARVG[0]) or die "Cannot open the file: $!";
-while (my $line = <BED_FILE>) {
-	push <BED_FILE>, $_;
-	my @column = split(" ",$line );
-	if ( $column[1] => $column[2] ) {
-        push( @output, "$column[0]\t$column[2]\t$column[1]\t$column[3]" );
+open(DATA, $ARGV[0]) or die "Cannot open the file: $!";
+
+while (<DATA>) {
+    push @input, $_;
+}
+for (@input) {
+    my @column = split( /\s+/, $_ );
+
+    if ( $column[1] <= $column[2] ) {
+        push( @output, "$column[0] $column[1] $column[2] $column[3]" );
     }
-	else {
-	push @output, "$column[0]\t$column[1]\t$column[2]\t$column[3]";
+    else {
+        push ( @output, "$column[0] $column[2] $column[1] $column[3]");
     }
 }
 
 print "$_\n" for @output
+
